de7b241962de079b270ebe8014bf51a41262e81e
GuSt4v0CCAM4 <alonsospinola70@gmail.com>
2023-07-01 11:38:03 -0500

    27/06/2023

--- DBMS/Disco.h
+++ DBMS/Disco.h
@@ -8,45 +8,54 @@
 #include "sstream"
 #include "string"
 #include "vector"
-//un comentario
+
 using namespace std;
-namespace fs = filesystem;
+namespace fs = filesystem; // fs se llama a la librería de filesystem
 class Disco {
 private:
     int capacidadDisco, capacidadSector, numSectores, numPistas, numSuperficies=2, numPlatos;
-    string diskname;
+    string diskname; //el nombre que tiene la carpeta que hace como disco
 public:
+    //constructor para guardar los diferente valores del disco
     Disco(int capacidadSector, int numSectores, int numPistas,  int numPlatos) :
     capacidadSector(capacidadSector), numSectores(numSectores), numPistas(numPistas), numPlatos(numPlatos)
     {
         capacidadDisco = capacidadSector * numSectores * numPistas * numPistas * numPlatos;
 
-// UN COMENTAARIO
-// Segundo comentario
+
     }
-    bool diskExists(const string& diskName)
+    bool diskExists(const string& diskName) //verifica si el disco existe
     {
         return fs::exists(diskName);
     }
-    void createFolder(string diskname){
-        if (diskExists(diskname)) {
+    void createFolder(string diskname){ //crea la carpeta
+        if (diskExists(diskname)) {    //llamamos a la funcion de verificar si el disco existe
             cout << "El disco ya existe." << endl;
             return;
         }else{
+            // creamos las caarpetas, subcarpetas y archivos de txt
             fs::create_directory(diskname);
             for (int plato = 0; plato < numPlatos; ++plato) {
+                //definimos  la ubicacion de la carpeta, iterando segun nuemro de platos
+                // sumamos +1 a plato ya que comenzamos desde 0 en la iteracion
                 string carpetaPlato = diskname + "/Plato " + to_string(plato + 1);
                 fs::create_directory(carpetaPlato);
 
                 for (int superficie = 0; superficie < numSuperficies; ++superficie) {
+                    //definimos  la ubicacion de la carpeta, iterando segun nuemro de superficies
+                      //sumamos +1 a superficie ya que comenzamos desde 0 en la iteracion
                     string carpetaSuperficie = carpetaPlato + "/Superficie " + to_string(superficie + 1);
                     fs::create_directory(carpetaSuperficie);
 
                     for (int pista = 0; pista < numPistas; ++pista) {
+                        //definimos  la ubicacion de la carpeta, iterando segun nuemro de pista
+                        //sumamos +1 a pista ya que comenzamos desde 0 en la iteracion
                         string carpetaPista = carpetaSuperficie + "/Pista " + to_string(pista + 1);
                         fs::create_directory(carpetaPista);
                         for(int sector = 0; sector < numSectores; ++sector){
                             string position;
+                            //segun los valores de numSectores, numPistas, numSuperficies, numPlatos actuales
+                            //creamos la posicion del sector que tambien funcionara como nombre del archivo txt
                             position += (plato < 9 ? "0" : "") + to_string(plato + 1);
                             position += (superficie < 9 ? "0" : "") + to_string(superficie + 1);
                             position += (pista < 9 ? "0" : "") + to_string(pista + 1);
@@ -59,6 +68,8 @@ public:
                 }
             }
         }
+        //al crear el disco creamos el archivo con los valores de capacidad, numSectores, numPistas, numSuperficies, numPlatos
+        // y lo guardamos en un archivo de texto con el mismo nombre de la carpeta del disco
         ofstream file;
         file.open(diskname + "/" + diskname + ".txt", ios::out);
         file << capacidadSector << " " << numSectores << " " << numPistas << " " << numSuperficies << " " <<numPlatos << endl;
